4d1faf67af317f7a685a20e777f31cefc9790950
Joey Mathis <joseph.e.mathis@vanderbilt.edu>
2021-10-03 20:44:32 -0500

    add user creation

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
 
   get 'users/availability'
   get 'users/profile'
+
+  get 'signup', to: 'users#new'
+  post 'signup', to: 'users#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
