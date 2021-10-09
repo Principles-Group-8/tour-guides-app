61bf96d93be0d523556f204e0792459f9f6a1e20
Joey Mathis <joseph.e.mathis@vanderbilt.edu>
2021-10-09 15:49:36 -0500

    fix style tag error, start tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -10,4 +10,38 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     get users_profile_url
     assert_response :success
   end
+
+  test "should signup user" do
+    get signup_url
+    assert_response :success
+    post signup_url,
+      params:  { user: { email: "test@mail.com", password:"password" } }
+    assert User.find_by_email("test@mail.com")
+    assert_not User.find_by_email("test@mail.com").administrator
+  end
+
+  test "should error user" do
+    post signup_url,
+      params: { user: { email: "test", password: "password" } }
+    assert_not User.find_by_email("test")
+  end
+
+  test "should login user" do
+    post login_url,
+      params: { email: "normal@mail.com", password: 'password' }
+    assert session[:user_id] == User.find_by_email("normal@mail.com").id
+    get users_profile_url
+    assert_response :success
+  end
+
+  test "should fail login" do
+    post login_url,
+      params: { email: "normal@mail.com", password: "wrongpassword" }
+    assert_not session[:user_id]
+  end
+
+  test "should get list for administrator" do
+    
+  end
+
 end
