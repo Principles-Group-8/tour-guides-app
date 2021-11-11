61f9559975d712dc75315f86365ce4a1d71ce958
Joey Mathis <joseph.e.mathis@vanderbilt.edu>
2021-11-11 16:14:56 -0600

    Fix migration

--- db/migrate/20211026035106_add_end_time_and_tour_guides_to_tours.rb
+++ db/migrate/20211026035106_add_end_time_and_tour_guides_to_tours.rb
@@ -1,6 +1,6 @@
 class AddEndTimeAndTourGuidesToTours < ActiveRecord::Migration[6.1]
   def change
     add_column :tours, :end_time, :datetime
-    add_column :tours, :min_guides, :datetime, default: 10, null: false
+    add_column :tours, :min_guides, :datetime, default: Time.zone.now, null: false
   end
 end
--- db/migrate/20211027013518_fix_tour_time_guides.rb
+++ db/migrate/20211027013518_fix_tour_time_guides.rb
@@ -1,5 +1,5 @@
 class FixTourTimeGuides < ActiveRecord::Migration[6.1]
   def change
-    change_column :tours, :min_guides, :integer
+    change_column :tours, :min_guides, :integer, default: 10
   end
 end
