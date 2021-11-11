5dd4778c277be02d6e29dc15e52657c9319d3120
Joey Mathis <joseph.e.mathis@vanderbilt.edu>
2021-11-10 23:03:40 -0600

    Consolidate admin checking code

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -1,16 +1,12 @@
 class ToursController < ApplicationController
 
     def new
-        if !session[:user_id] || !User.find(session[:user_id]).administrator
-            redirect_to root_path
-        end
+        check_admin()
         @tour = Tour.new
     end
 
     def create
-        if !session[:user_id] || !User.find(session[:user_id]).administrator
-            redirect_to root_path
-        end
+        check_admin()
         @tour = Tour.new(tour_params)
         @tour.end_time = @tour.time + params[:tour][:hours].to_i.hours
         @tour.save
@@ -19,21 +15,23 @@ class ToursController < ApplicationController
     end
 
     def delete
-        if !session[:user_id] || !User.find(session[:user_id]).administrator
-            redirect_to root_path
-        end
+        check_admin()
         Tour.find(params[:id]).delete
         redirect_back(fallback_location: root_path)
     end
 
     def manage_guides
-        if !session[:user_id] || !User.find(session[:user_id]).administrator
-            redirect_to root_path
-        end
+        check_admin()
         @tour = Tour.find(params[:id])
     end
 
     private
+
+    def check_admin
+        if !session[:user_id] || !User.find(session[:user_id]).administrator
+            redirect_to root_path
+        end
+    end
 
     def tour_params
         params.require(:tour).except(:hours).permit(:time, :min_guides, :location, :note, :weekly, :weeks)
