5dde1ff9fbdb994717e4629a4c1f79e95019d8f3
GarrettCrumb <garrett.t.crumb@vanderbilt.edu>
2021-11-11 18:33:08 -0600

    Update tour tests

--- test/controllers/tours_controller_test.rb
+++ test/controllers/tours_controller_test.rb
@@ -2,16 +2,22 @@ require "test_helper"
 
 class ToursControllerTest < ActionDispatch::IntegrationTest
   test "should get new tour" do
+    post login_url,
+      params: { email: "admin@mail.com", password: 'password' }
     get tours_new_url
-    assert_response :found
+    assert_response :success
   end
 
   test "should get tour manager" do
+    post login_url,
+      params: { email: "admin@mail.com", password: 'password' }
     get tours_manage_url
-    assert_response :ok
+    assert_response :success
   end
 
   test "should create new tour" do
+    post login_url,
+      params: { email: "admin@mail.com", password: 'password' }
     post tours_create_url,
       params:  { tour: { time: "2022-02-11T16:31", hours: 1, min_guides: 5, location: "HOG", note: "testing", weekly: 0 } }
     assert Tour.find_by_time("2022-02-11T16:31")
