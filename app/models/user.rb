546dce295c9f46691c1d40814d2a89c40181a1bd
Joey Mathis <joseph.e.mathis@vanderbilt.edu>
2021-12-04 18:34:38 -0600

    Add availability helper method

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,20 @@ class User < ApplicationRecord
           update(false_hash)
     end
 
+    def has_availability
+      ["mon", "tues", "wed", "thur", "fri", "sat", "sun"].each do |day|
+        ["_9", "_9:30", "_10", "_10:30", "_11", 
+          "_11:30", "_12", "_12:30", "_1", "_1:30",
+          "_2", "_2:30", "_3", "_3:30", "_4", "_4:30",
+          "_5", "_5:30"].each do |time|
+          if send("#{day}#{time}")
+            return true
+          end
+        end
+      end
+      false
+    end
+
     def name_display
       "#{first_name} #{last_name}"
     end
