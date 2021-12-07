9ff8cca47b40b92f0397ee23d50981c172d987a2
Joey Mathis <joseph.e.mathis@vanderbilt.edu>
2021-12-06 18:40:52 -0600

    update tests

--- test/controllers/tours_controller_test.rb
+++ test/controllers/tours_controller_test.rb
@@ -3,14 +3,14 @@ require "test_helper"
 class ToursControllerTest < ActionDispatch::IntegrationTest
   test "should get new tour" do
     post login_url,
-      params: { email: "admin@mail.com", password: 'password' }
+      params: { email: "admin@vanderbilt.edu", password: 'password' }
     get tours_new_url
     assert_response :success
   end
 
   test "should not get new tour for non-administrator" do
     post login_url,
-      params: { email: "normal@mail.com", password: 'password' }
+      params: { email: "normal@vanderbilt.edu", password: 'password' }
     get tours_new_url
     assert_response :redirect
   end
@@ -22,14 +22,14 @@ class ToursControllerTest < ActionDispatch::IntegrationTest
 
   test "should get tour manager" do
     post login_url,
-      params: { email: "admin@mail.com", password: 'password' }
+      params: { email: "admin@vanderbilt.edu", password: 'password' }
     get tours_manage_url
     assert_response :success
   end
 
   test "should not get tour manager for non-administrator" do
     post login_url,
-      params: { email: "normal@mail.com", password: 'password' }
+      params: { email: "normal@vanderbilt.edu", password: 'password' }
     get tours_manage_url
     assert_response :redirect
   end
@@ -41,7 +41,7 @@ class ToursControllerTest < ActionDispatch::IntegrationTest
 
   test "should create new tour" do
     post login_url,
-      params: { email: "admin@mail.com", password: 'password' }
+      params: { email: "admin@vanderbilt.edu", password: 'password' }
     post tours_create_url,
       params:  { tour: { time: "2022-02-11T16:31", hours: 1, min_guides: 5, location: "HOG", note: "testing", weekly: 0 } }
     assert Tour.find_by_time("2022-02-11T16:31")
@@ -52,7 +52,7 @@ class ToursControllerTest < ActionDispatch::IntegrationTest
     tour = Tour.new(time: time, end_time: time + 1.hours, min_guides: 10, location: "HOG")
     assert tour.save
     post login_url,
-      params: { email: "admin@mail.com", password: "password" }
+      params: { email: "admin@vanderbilt.edu", password: "password" }
     get "/tours/#{tour.id}"
     assert_response :redirect
     assert_raises(ActiveRecord::RecordNotFound) do
